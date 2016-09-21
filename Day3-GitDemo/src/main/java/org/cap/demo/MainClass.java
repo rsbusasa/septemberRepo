709debb8b4da24f2261c9bcc2bdeec3667f8b3ac
viji lakshmi <vijayalakshmi.david@capgemini.com>
2016-09-21 12:22:55 +0530

    Day3-GitDemo-First Commit

---
+++ Day3-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,16 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+	
+		SalesClass obj=new SalesClass();
+		obj.display();
+		greetings();
+	}
+
+	
+	public static void greetings(){
+		System.out.println("Hello World! Welcome To Git Repository");
+	}
+}
---
+++ Day3-GitDemo/src/main/java/org/cap/demo/SalesClass.java
@@ -0,0 +1,11 @@
+package org.cap.demo;
+
+public class SalesClass {
+	
+	int[] arr={1,2,3,45};
+	
+	public void display(){
+		System.out.println("Sales Class Display method");
+	}
+
+}
