73eda7cde924a681b3087d0e3b6365948890bc24
viji lakshmi <vijayalakshmi.david@capgemini.com>
2016-09-22 10:10:23 +0530

    greetings method updated,

--- Day3-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day3-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -13,5 +13,6 @@ public class MainClass {
 	public static void greetings(){
 		System.out.println("Hello World! Welcome To Git Repository");
 		System.out.println("Test 1 - Repo Test");
+		System.out.println("Greetings Method New Branch Added.");
 	}
 }
